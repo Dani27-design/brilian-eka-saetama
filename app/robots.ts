dcffedccb373e47e4bcfd660f9d8ab1503e278aa
coboy <daniansyahchusyaidin@gmail.com>
2025-06-11 00:42:18 +0700

    update seo blog

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/admin/", "/api/"],
+        disallow: ["/admin/"],
       },
     ],
     sitemap: ["https://brilian-eka-saetama.vercel.app/sitemap.xml"],
